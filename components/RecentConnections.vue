e14a546254ca671e377cf3371290914c5f05299a
Ben <allobon@gmail.com>
2020-01-13 23:01:36 +0000

    get ready for connectivity

--- components/RecentConnections.vue
+++ components/RecentConnections.vue
@@ -5,7 +5,31 @@
 </template>
 
 <script>
+
+import { mapGetters }					from "vuex";
+import io 										from "socket.io-client";
+
 export default {
+  data(){
+    return {
+      socket: io("http://localhost:1337")
+    }
+  },
+  methods: {
+    init(){
+      console.log(this.socket)
+      this.socket.on("user-action", (actionData) => {
+				this.handleUserAction(actionData);
+			});
+    },
+    handleUserAction(actionData){
+      console.log("user action here");
+      console.log(actionData);
+    }
+  },
+  mounted(){
+    this.init();
+  }
 }
 </script>
 
